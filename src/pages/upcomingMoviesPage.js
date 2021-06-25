0b0639c2074b730b6fe50b44ad96fce65c6271ed
dugganj780 <dugganj780@gmail.com>
2021-06-25 17:03:58 +0100

    Change upcoming movies page to use caching.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -5,6 +5,12 @@ import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
 import AddToMustWatchIcon from '../components/cardIcons/addToMustWatch';
 
+/*
+To complete Exercise 1 I edited my Upcoming Movies page to match the Home Page.
+I also updated the API call for getUpcomingMovies.
+This also completed Exercise 3. I am adding this comment so that I can save the changes and push to git.
+*/
+
 const UpcomingMoviesPage = (props) => {
   const { data, error, isLoading, isError } = useQuery('upcoming', getUpcomingMovies)
 
